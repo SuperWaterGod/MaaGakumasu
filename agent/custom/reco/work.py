c933acf9a78dc6d972463e9511417f441a7a6730
SuperWaterGod <yancaiwudi@foxmail.com>
2025-05-24 20:25:42 +0300

    feat:工作自动选择笑脸/最高好感

--- agent/custom/reco/work.py
+++ agent/custom/reco/work.py
@@ -1,3 +1,4 @@
+import time
 from utils import logger
 from typing import Union, Optional
 
@@ -10,7 +11,8 @@ from maa.custom_recognition import CustomRecognition
 @AgentServer.custom_recognition("WorkChooseAuto")
 class WorkChooseAuto(CustomRecognition):
     """
-        识别选择工作
+        自动识别选择工作
+        优先选择笑脸，没有笑脸则按照好感度选择
     """
 
     def analyze(
@@ -18,43 +20,116 @@ class WorkChooseAuto(CustomRecognition):
             context: Context,
             argv: CustomRecognition.AnalyzeArg,
     ) -> Union[CustomRecognition.AnalyzeResult, Optional[RectType]]:
-        logger.info("开始识别")
-        reco_detail = context.run_recognition(
-            "WorkIdolAffinity",
-            argv.image,
-            pipeline_override={"WorkIdolAffinity":
-                {
-                    "recognition": "OCR",
-                    "expected": "^(?:0|[1-9]|1[0-9]|20)20$",
-                    "roi": [70, 788, 558, 240],
-                    "threshold": 0.3
-                }
-            },
-        )
-        if reco_detail is None:
-            logger.info("识别失败")
-            return CustomRecognition.AnalyzeResult(box=None, detail="无文字")
+
+        reco_detail = context.run_recognition("WorkChooseGood", argv.image,
+                                              pipeline_override={"WorkChooseGood": {
+                                                  "roi": [8, 709, 621, 317]
+                                              }})
+        if reco_detail:
+            logger.info("第一页有笑脸")
+
+            good_list = []
+            for result in reco_detail.filterd_results:
+                good_list.append(result.box)
+
+            image = context.tasker.controller.post_screencap().wait().get()
+            reco_detail = context.run_recognition(
+                "WorkAlready", image,
+                pipeline_override={"WorkAlready": {
+                    "roi": [good_list[0][0], good_list[0][1], 150, 150]
+                }})
+
+            if reco_detail:
+                if len(good_list) > 1:
+                    logger.info("笑脸存在且被选中, 选择第二个笑脸")
+                    return CustomRecognition.AnalyzeResult(box=good_list[1], detail="笑脸存在且被选中, 选择第二个笑脸")
+                else:
+                    logger.info("笑脸存在且被选中, 左滑动")
+                    context.tasker.controller.post_swipe(400, 864, 200, 864, duration=200).wait()
+                    time.sleep(0.5)
+            else:
+                logger.info("笑脸存在且未被选中")
+                return CustomRecognition.AnalyzeResult(box=good_list[0], detail="笑脸存在且未被选中")
+
         else:
+            logger.info("第一页无笑脸, 左滑动")
+            context.tasker.controller.post_swipe(400, 864, 200, 864, duration=200).wait()
+            time.sleep(0.5)
+
+        image = context.tasker.controller.post_screencap().wait().get()
+        reco_detail = context.run_recognition("WorkChooseGood", image,
+                                              pipeline_override={"WorkChooseGood": {
+                                                  "roi": [104, 719, 615, 309]
+                                              }})
+        if reco_detail:
+            logger.info("第二页有笑脸")
+
+            good_list = []
+            for result in reco_detail.filterd_results:
+                good_list.append(result.box)
+            image = context.tasker.controller.post_screencap().wait().get()
+            reco_detail = context.run_recognition(
+                "WorkAlready", image,
+                pipeline_override={"WorkAlready": {
+                    "roi": [good_list[0][0], good_list[0][1], 150, 150]
+                }})
+            if reco_detail:
+                if len(good_list) > 1:
+                    logger.info("笑脸存在且被选中, 选择第二个笑脸")
+                    return CustomRecognition.AnalyzeResult(box=good_list[1], detail="笑脸存在且被选中, 选择第二个笑脸")
+                else:
+                    logger.info("笑脸存在且被选中, 右滑动")
+                    context.tasker.controller.post_swipe(200, 864, 400, 864, duration=200).wait()
+                    time.sleep(0.5)
+            else:
+                logger.info("笑脸存在且未被选中")
+                return CustomRecognition.AnalyzeResult(box=good_list[0], detail="笑脸存在且未被选中")
+
+        else:
+            logger.info("第二页无笑脸，右滑动")
+            context.tasker.controller.post_swipe(200, 864, 400, 864, duration=200).wait()
+            time.sleep(0.5)
+        image = context.tasker.controller.post_screencap().wait().get()
+        reco_detail = context.run_recognition(
+            "WorkIdolAffinity", image,
+            pipeline_override={"WorkIdolAffinity": {
+                "recognition": "OCR",
+                "expected": "^(0|[1-9]|1[0-9]|20)\\/20$",
+                "roi": [70, 788, 558, 240]}
+            })
+
+        if reco_detail:
             affinity_list = []
             for result in reco_detail.filterd_results:
-
-                affinity_level = 0
-                logger.info(result)
-                if len(result.text) < 3:
-                    continue
-                elif len(result.text) == 3:
-                    affinity_level = result.text.replace("20", "")
-                elif len(result.text) == 4:
-                    affinity_level = result.text.replace("120", "")
-                else:
-                    continue
                 affinity_list.append({
                     "box": result.box,
-                    "text": affinity_level
+                    "text": int(result.text.replace("/20", ""))
                 })
-            max_affinity = max(affinity_list, key=lambda item: int(item['text']))
-            print(max_affinity)
-        """# context is a reference, will override the pipeline for whole task
+            sorted_list = sorted(affinity_list, key=lambda item: item['text'])
+            max_affinity = sorted_list[-1]
+            second_affinity = sorted_list[-2]
+            max_affinity_box = max_affinity["box"]
+
+            image = context.tasker.controller.post_screencap().wait().get()
+            reco_detail = context.run_recognition(
+                "WorkAlready", image,
+                pipeline_override={"WorkAlready": {
+                    "roi": [max_affinity_box[0] - 90, max_affinity_box[1] - 110, 150, 150]
+                }})
+
+            if reco_detail:
+                logger.info("最高好感已工作")
+                return CustomRecognition.AnalyzeResult(box=second_affinity["box"], detail="第二高好感度")
+            else:
+                logger.info("最高好感未工作")
+                return CustomRecognition.AnalyzeResult(box=max_affinity["box"], detail="最高好感度")
+        else:
+            logger.info("OCR识别失败")
+            return CustomRecognition.AnalyzeResult(box=None, detail="无文字")
+
+
+"""
+        # context is a reference, will override the pipeline for whole task
         context.override_pipeline({"MyCustomOCR": {"roi": [1, 1, 114, 514]}})
         # context.run_recognition ...
 
@@ -69,6 +144,3 @@ class WorkChooseAuto(CustomRecognition):
 
         context.override_next(argv.node_name, ["TaskA", "TaskB"])
 """
-        return CustomRecognition.AnalyzeResult(
-            box=(0, 0, 100, 100), detail="Hello World!"
-        )
